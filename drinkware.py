8b181eb35cdda407bf53a19f827dd51d9060b4a2
m4rsm4ps <stonedwelder@gmail.com>
2025-05-28 16:52:30 +0300

    moved essetial method to the parent class in drinkware.py

--- drinkware.py
+++ drinkware.py
@@ -3,10 +3,21 @@ from beverages import *
 
 
 class Vessel:
-    
+
     def __init__(self, volume: int, contents: Beverage | None) -> None:
         self.volume = volume
         self.contents = contents
+
+    def pour(self, amount: int) -> Beverage:
+        if amount > self.contents.amount:
+            raise ValueError("Not enough contents!")
+        self.contents.amount -= amount
+        return type(self.contents)(**self.contents.get_properties(), amount=amount)
+
+    def pour_in(self, beverage:Beverage, amount: int) -> None:
+        if self.contents.amount + amount > self.volume:
+            raise ValueError("Contents amount cannot exceed Vessel volume!")
+        self.contents.amount += amount
 
 
 class Glass:
@@ -32,18 +43,13 @@ class Bottle(Vessel):
     def pour(self, amount: int) -> Beverage:
         if self.sealed:
             raise AttributeError("The Bottle is sealed, cannot pour!")
-        if amount > self.contents.amount:
-            raise ValueError("Not enough contents!")
-        self.contents.amount -= amount
-        return type(self.contents)(**self.contents.get_properties(), amount=amount)
+        return super().pour(amount)
 
     def pour_in(self, beverage:Beverage, amount: int) -> None:
         if beverage.get_properties() != self.contents.get_properties():
             raise ValueError("Mixing beverages in Bottle is not allowed!")
-        if self.contents.amount + amount > self.volume:
-            raise ValueError("Contents amount cannot exceed volume of Bottle!")
-        self.contents.amount += amount
-
+        return super().pour_in(beverage, amount)
+ 
     def get_contents_stock_props(self):
         beverage_props = self.contents.get_properties()
         contents_stock_props = {**beverage_props, **dict(amount=self.volume)}
